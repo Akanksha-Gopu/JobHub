543f169cc97181a54a8f65b0dc711bdc87da76f8
Akanksha <akankshareddy891@gmail.com>
2026-07-03 00:32:35 +0530

    Fix Neon endpoint configuration

--- api/utils/pdo.php
+++ api/utils/pdo.php
@@ -6,9 +6,10 @@ require_once __DIR__ . "/../config.php";
 function getPDO() {
     try {
          $dsn = "pgsql:host=" . DB_HOST .
-               ";port=" . DB_PORT .
-               ";dbname=" . DB_NAME .
-               ";sslmode=require";
+       ";port=" . DB_PORT .
+       ";dbname=" . DB_NAME .
+       ";sslmode=require" .
+       ";options=endpoint=" . getenv("NEON_ENDPOINT_ID");
         $pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
